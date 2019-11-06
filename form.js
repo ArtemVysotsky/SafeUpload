0eb1a3b2a28996916bd171bc1d236b929e523585
Artem Visotsky <a.vysotsky@gmail.com>
2019-11-06 21:45:05 +0200

    ref upload form (begin 2)

--- form.js
+++ form.js
@@ -7,131 +7,94 @@
  * @copyright   Всі права застережено (c) 2019 Upload
  */
 
+let uploadForm = new function() {
+    let self = this;
+    this.upload = {};
+    this.limit = (100 * 1048576);
 
-/*let limit = 100 * 1048576;
-*/
-
-let file;
-let uploadForm = function() {
-    let upload, timerID;
-    let nodes = function() {
-        this.form = this.fileButton = this.progressBar = this.uploadButton = this.pauseButton =
-        this.resumeButton = this.cancelButton = this.sizeIndicator = this.speedIndicator = this.timeIndicator = null;
+    this.chooseEvent = function(file) {
+        if (file === undefined) return false;
+        if (file.size > this.limit) {
+            this.alert.show('Розмір файлу більше допустимого');
+            return;
+        }
+        this.reset();
+        this.button.enable('upload');
+        this.indicators.size(file.size);
+        this.upload = new Upload(file, {timeout: 3000, retry: {interval: 3000, limit: 5}});
+        this.upload.addListener('done', function() {self.doneEvent();});
+        this.upload.addListener('fail', this.failEvent);
+        console.log(this.upload);
     };
-    done = function() {
-        button.enable('file').disable(['pause', 'resume', 'cancel']);
-        timer.stop();
-        indicators.update();
+    this.startEvent = function() {
+        this.button.enable(['pause', 'cancel']).disable('upload');
+        this.timer.start();
+        this.upload.start();
     };
-    fail = function() {
-        button.enable('file').disable(['upload', 'pause', 'resume', 'cancel']);
-        timer.stop();
-        indicators.update();
-        alert.show('Помилка! ' + upload.getError());
+    this.pauseEvent = function() {
+        this.button.enable('resume').disable('pause');
+        this.upload.pause();
     };
-    start = function() {
-        button.enable(['pause', 'cancel']).disable('upload');
-        timer.start();
-        upload.start();
+    this.resumeEvent = function() {
+        this.button.enable(['file', 'pause']).disable('resume');
+        this.upload.resume();
     };
-    pause = function() {
-        button.enable('resume').disable('pause');
-        upload.pause();
+    this.stopEvent = function() {
+        this.reset();
+        this.timer.stop();
+        this.indicators.update();
+        this.button.enable('file').disable(['upload', 'pause', 'resume', 'cancel']);
+        this.indicators.clear();
+        this.upload.stop();
     };
-    resume = function() {
-        button.enable(['file', 'pause']).disable('resume');
-        upload.resume();
+    this.doneEvent = function() {
+        this.button.enable('file').disable(['pause', 'resume', 'cancel']);
+        this.timer.stop();
+        this.indicators.update();
     };
-    stop = function() {
-        reset();
-        timer.stop();
-        indicators.update();
-        button.enable('file').disable(['upload', 'pause', 'resume', 'cancel']);
-        indicators.clear();
-        upload.stop();
+    this.failEvent = function() {
+        this.button.enable('file').disable(['upload', 'pause', 'resume', 'cancel']);
+        this.timer.stop();
+        this.indicators.update();
+        this.alert.show('Помилка! ' + this.upload.getError());
     };
-    button = function() {
+    this.button = function() {
         this.enable = function(title) {
             return title;
         };
         this.disable = function(title) {
             return title;
-        };
+        }
     };
-    indicators = function() {
-        this.size = function(value) {nodes.sizeIndicator.text('('+Human.size(value)+')');};
+    this.indicators = function() {
+        this.size = function(value) {self.nodes.sizeIndicator.text('('+Human.size(value)+')');};
         this.update = function() {
-            let indicators = upload.getIndicators();
-            nodes.progressBar
+            let indicators = self.upload.getIndicators();
+            self.nodes.progressBar
                 .css('width', indicators.percent + '%')
                 .text(Human.size(indicators.size) + ' (' + indicators.percent + '%)');
-            nodes.speedIndicator.text(
+            self.nodes.speedIndicator.text(
                 Human.size(indicators.speed) + '/c' + ' (' + Human.size(indicators.chunk) + ')'
             );
-            nodes.timeIndicator.text(
+            self.nodes.timeIndicator.text(
                 Human.time(indicators.timeElapsed) + ' / ' + Human.time(indicators.timeEstimate)
             );
         };
         this.clear = function() {
-            nodes.sizeIndicator.text(null);
-            nodes.speedIndicator.text(null);
-            nodes.timeIndicator.text(null);
-            nodes.progressBar.css('width', 0).text(null);
-        };
+            self.nodes.sizeIndicator.text(null);
+            self.nodes.speedIndicator.text(null);
+            self.nodes.timeIndicator.text(null);
+            self.nodes.progressBar.css('width', 0).text(null);
+        }
     };
-    timer = function() {
-        this.start = function() {timerID = setInterval(indicators.update, 1000);};
-        this.stop = function() {clearInterval(timerID);};
+    this.timer = function() {
+        this.start = function() {self.timerID = setInterval(self.indicators.update, 1000);};
+        this.stop = function() {clearInterval(self.timerID);};
     };
-    alert = function() {
-        this.show = function(message) {nodes.alert.text(message).show();};
-        this.hide = function() {nodes.alert.hide();};
+    this.alert = new function() {
+        this.show = function(message) {self.nodes.alert.text(message).show();};
+        this.hide = function() {self.nodes.alert.hide();};
     };
-    reset = function() {nodes.form[0].reset();};
+    this.reset = function() {this.nodes.form[0].reset();}
 };
-console.log(uploadForm.Prototype);
-uploadForm.prototype.nodes = function() {
-    this.main = $('main');
-    this.alert = this.main.find('div.alert');
-    this.form = this.main.find('form');
-    this.fileForm = this.form.find('div.file');
-    this.progressForm = this.form.find('div.progress');
-    this.controlForm = this.form.find('div.control');
-    this.statusForm = this.form.find('div.status');
-    this.fileButton = this.fileForm.find('input');
-    this.progressBar = this.progressForm.find('div.progress-bar');
-    this.uploadButton = this.controlForm.find('input.upload');
-    this.pauseButton = this.controlForm.find('input.pause');
-    this.resumeButton = this.controlForm.find('input.resume');
-    this.cancelButton = this.controlForm.find('input.cancel');
-    this.sizeIndicator = this.fileForm.find('span');
-    this.speedIndicator = this.statusForm.find('span.speed');
-    this.timeIndicator = this.statusForm.find('span.time');
-};
-uploadForm.prototype.nodes.form.show();
-
-uploadForm.prototype.nodes.fileButton.change(function() {
-    file = $(this)[0].files[0];
-    if (file === undefined) return false;
-    if (file.size > parent.limit) {
-        alert.show('Розмір файлу більше допустимого');
-        return;
-    }
-    reset();
-    button.enable('upload');
-    indicators.size(file.size);
-    upload = new Upload(file, {timeout: 3000, retry: {interval: 3000, limit: 5}});
-    upload.addListener('done', function() {
-        uploadForm.done();
-    });
-    upload.addListener('fail', function() {
-        uploadForm.fail();
-    });
-});
-
-uploadForm.prototype.nodes.uploadButton.click(function() {start()});
-uploadForm.prototype.nodes.pauseButton.click(function() {pause()});
-uploadForm.prototype.nodes.resumeButton.click(function() {resume()});
-uploadForm.prototype.nodes.cancelButton.click(function() {stop()});
-
 
--- index.js
+++ index.js
@@ -8,12 +8,54 @@
  */
 
 $(document).ready(function() {
-    let script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = '/form.js?v=1';
-    document.head.appendChild(script);
-});
 
+    console.log(uploadForm);
+
+    uploadForm.nodes = new function() {
+        this.main = $('main');
+        this.alert = this.main.find('div.alert');
+        this.card = this.main.find('div.card');
+        this.form = this.card.find('div.card-body form');
+        this.fileForm = this.form.find('div.file');
+        this.progressForm = this.form.find('div.progress');
+        this.controlForm = this.form.find('div.control');
+        this.statusForm = this.form.find('div.status');
+        this.fileInput = this.fileForm.find('input');
+        this.progressBar = this.progressForm.find('div.progress-bar');
+        this.uploadButton = this.controlForm.find('input.upload');
+        this.pauseButton = this.controlForm.find('input.pause');
+        this.resumeButton = this.controlForm.find('input.resume');
+        this.cancelButton = this.controlForm.find('input.cancel');
+        this.sizeIndicator = this.fileForm.find('span');
+        this.speedIndicator = this.statusForm.find('span.speed');
+        this.timeIndicator = this.statusForm.find('span.time');
+    };
+
+    console.log(uploadForm.nodes);
+    console.log(uploadForm.alert);
+    console.log(uploadForm.alert.hide);
+
+//uploadForm.alert.hide();
+    uploadForm.nodes.card.show();
+
+    uploadForm.triggers = new function() {
+        console.log(this);
+        this.nodes.fileInput.change(function() {this.chooseEvent($(this)[0].files[0]);});
+        this.nodes.uploadButton.click(function() {this.startEvent()});
+        this.nodes.pauseButton.click(function() {this.pauseEvent()});
+        this.nodes.resumeButton.click(function() {this.resumeEvent()});
+        this.nodes.cancelButton.click(function() {this.stopEvent()});
+        this.nodes.alert.click(this.alert.hide);
+    };
+/*
+    uploadForm.nodes.fileInput.change(function() {uploadForm.chooseEvent($(this)[0].files[0]);});
+    uploadForm.nodes.uploadButton.click(function() {uploadForm.startEvent()});
+    uploadForm.nodes.pauseButton.click(function() {uploadForm.pauseEvent()});
+    uploadForm.nodes.resumeButton.click(function() {uploadForm.resumeEvent()});
+    uploadForm.nodes.cancelButton.click(function() {uploadForm.stopEvent()});
+    uploadForm.nodes.alert.click(uploadForm.alert.hide);
+*/
+});
 
 class Human {
     static size(bytes) {
