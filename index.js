76035779596261a37500be525919aa243d0bd006
Artem Visotsky <a.vysotsky@gmail.com>
2020-01-31 01:43:40 +0200

    ref nodes, timers, callbacks

--- index.js
+++ index.js
@@ -6,83 +6,115 @@
  * @copyright   GNU General Public License v3
  */
 
-/** ToDo: Переробити таймери та інтервали в об'єкт разом зі ідентифікаторами */
-
 $(document).ready(function() {
-    let file, // об'єкт файл форми завантаження
-        upload, // об'єкт для здійснення завантаження
-        nodes = {}, // збережені посилання на елементи сторінки
-        timer = {status: {}, size: null}, // мітки часу
-        interval = {
-            status: 1000, // інтервал оновляення статусу завантаження файлу, мілісекунди
-            size: 200 // інтервал оновляення розміру завантаження файлу, мілісекунди
+    let file, upload;
+
+    // Збережені посилання на елементи сторінки
+    const nodes = new function() {
+        const root = this;
+        root.main = $('main');
+        root.card = this.main.find('div.card');
+        root.form = new function() {
+            this.self = root.card.find('div.card-body form');
+            this.file = this.self.find('div.file');
+            this.progress = this.self.find('div.progress');
+            this.control = this.self.find('div.control');
+            this.status = this.self.find('div.status');
         };
-    nodes.main = $('main');
-    nodes.card = nodes.main.find('div.card');
-    nodes.form = {};
-    nodes.form.self = nodes.card.find('div.card-body form');
-    nodes.form.file = nodes.form.self.find('div.file');
-    nodes.form.progress = nodes.form.self.find('div.progress');
-    nodes.form.control = nodes.form.self.find('div.control');
-    nodes.form.status = nodes.form.self.find('div.status');
-    nodes.buttons = {};
-    nodes.buttons.file = nodes.form.file.find('input');
-    nodes.buttons.upload = nodes.form.control.find('input.upload');
-    nodes.buttons.pause = nodes.form.control.find('input.pause');
-    nodes.buttons.resume = nodes.form.control.find('input.resume');
-    nodes.buttons.cancel = nodes.form.control.find('input.cancel');
-    nodes.indicators = {};
-    nodes.indicators.size = nodes.form.file.find('span');
-    nodes.indicators.speed = nodes.form.status.find('span.speed');
-    nodes.indicators.time = nodes.form.status.find('span.time');
-    nodes.indicators.progress = nodes.form.progress.find('div.progress-bar');
+        root.buttons = new function() {
+            this.file = root.form.file.find('input');
+            this.upload = root.form.control.find('input.upload');
+            this.pause = root.form.control.find('input.pause');
+            this.resume = root.form.control.find('input.resume');
+            this.cancel = root.form.control.find('input.cancel');
+        };
+        root.indicators = new function() {
+            this.size = root.form.file.find('span');
+            this.speed = root.form.status.find('span.speed');
+            this.time = root.form.status.find('span.time');
+            this.progress = root.form.progress.find('div.progress-bar');
+        };
+    };
 
+    // Клас керування таймерами для оновлення інформації про процес завантаження файлу
+    let timer = new function() {
+        let timers = { // налаштування таймерів
+            status: { // таймер для оновлення статусу завантаження файлу
+                id: null, // номер таймера оновляення статусу
+                interval: 1000, // інтервал оновляення статусу, мілісекунди
+                code: () => { // код оновляення статусу
+                let status = upload.getStatus();
+                nodes.indicators.speed.text(
+                    human.size(status.speed) + '/c' + ' (' + human.size(status.chunk) + ')'
+                );
+                nodes.indicators.time.text(
+                    human.time(status.time.elapsed) + ' / ' + human.time(status.time.estimate)
+                );
+            }},
+            size: { // таймер оновлення розміру завантаження файлу
+                id: null, // номер таймера оновляення розміру
+                interval: 200, // інтервал оновляення розміру, мілісекунди
+                code: () => { // код оновляення розміру
+                let size = upload.getSize();
+                let percent = Math.round(size * 100 / file.size);
+                nodes.indicators.progress.css('width',  percent + '%')
+                    .text(human.size(size) + ' (' + percent + '%)');
+            }}
+        };
+        this.start = () => {
+            $.each(timers, function() {
+                this.id = setInterval(this.code, this.interval);
+            });
+        };
+        this.stop = () => {
+            $.each(timers, function() {
+                setTimeout(() => {clearInterval(this.id)}, this.interval);
+            });
+        }
+    };
 
-    let callbacks = {};
-    callbacks.start = () => { // Дії при запуску процесу завантаження файлу
-        nodes.buttons.file.disable();
-        nodes.buttons.upload.disable();
-        nodes.buttons.pause.enable();
-        nodes.buttons.cancel.enable();
-        // Запускаємо періодичне оновлення статусу та розміру завантаження файлу
-        timer.status = setInterval(Update.status, interval.status);
-        timer.size = setInterval(Update.size, interval.size);
-    };
-    callbacks.pause = () => { // Дії при призупиненні процесу завантаження файлу
-        nodes.buttons.resume.enable();
-        nodes.buttons.pause.disable();
-        setTimeout(() => {clearInterval(timer.status)}, interval.status);
-        setTimeout(() => {clearInterval(timer.size)}, interval.size);
-    };
-    callbacks.resume = () => { // Дії при продовжені процесу завантаження файлу
-        nodes.buttons.pause.enable();
-        nodes.buttons.resume.disable();
-        timer.status = setInterval(Update.status, interval.status);
-        timer.size = setInterval(Update.size, interval.size);
-    };
-    callbacks.stop = () => { // Дії при зупинці процесу завантаження файлу
-        nodes.buttons.file.enable();
-        nodes.buttons.pause.disable();
-        nodes.buttons.resume.disable();
-        nodes.buttons.cancel.disable();
-        setTimeout(() => {clearInterval(timer.status)}, interval.status);
-        setTimeout(() => {clearInterval(timer.size)}, interval.size);
-    };
-    callbacks.timeout = () => { // Дії при невдалому продовжені процесу завантаження файлу
-console.log('callbacks.timeout.fail');
-        callbacks.pause();
-        alert('Сервер не відповідає, спробуйте пізніше');
-    };
-    callbacks.upload = {
-        done: () => { // Дії при успішному завершенні процесу завантаженні файлу
-            console.log('Файл "' + file.name + '" завантажено вдало');
-        },
-        fail: () => { // Дії при невдалому виконанні процесу завантаженні файлу
+    // Створюємо дії на різні випадки процесу завантаження файлу
+    const callbacks = {
+        start: () => {
+            nodes.buttons.file.disable();
             nodes.buttons.upload.disable();
-            alert(upload.getMessage())
+            nodes.buttons.pause.enable();
+            nodes.buttons.cancel.enable();
+            timer.start();
         },
-        always: () => { // Дії при успішному завершенні та невдалому виконанні процесу завантаження файлу
-            callbacks.stop();
+        pause: () => {
+            nodes.buttons.resume.enable();
+            nodes.buttons.pause.disable();
+            timer.stop();
+        },
+        resume: () => {
+            nodes.buttons.pause.enable();
+            nodes.buttons.resume.disable();
+            timer.start();
+        },
+        stop: () => {
+            nodes.buttons.file.enable();
+            nodes.buttons.pause.disable();
+            nodes.buttons.resume.disable();
+            nodes.buttons.cancel.disable();
+            timer.stop();
+        },
+        timeout: () => {
+            console.log('callbacks.timeout.fail');
+            callbacks.pause();
+            alert('Сервер не відповідає, спробуйте пізніше');
+        },
+        upload: {
+            done: () => {
+                console.log('Файл "' + file.name + '" завантажено вдало');
+            },
+            fail: () => {
+                nodes.buttons.upload.disable();
+                alert(upload.getMessage())
+            },
+            always: () => {
+                callbacks.stop();
+            }
         }
     };
 
@@ -96,37 +128,17 @@ console.log('callbacks.timeout.fail');
         nodes.indicators.time.text('');
         nodes.indicators.progress.css('width', 0).text(null);
     });
+
     // Дії при запуску процесу завантаження файлу
     nodes.buttons.upload.click(() => {
-        // Створення об'єкту для завантаження файлу зі зворотніми функціями
         upload = new Upload(file, callbacks);
         upload.start();
     });
-    // Дії при призупиненні процесу завантаження файлу
-    nodes.buttons.pause.click(() => {upload.pause()});
-    // Дії при продовжені процесу завантаження файлу
-    nodes.buttons.resume.click(() => {upload.resume()});
-    // Дії при відміні процесу завантаження файлу
-    nodes.buttons.cancel.click(() => {upload.stop()});
 
-    // Клас оновлення стутусу та розміру завантаження файлу
-    class Update {
-        static status() {
-            let status = upload.getStatus();
-            nodes.indicators.speed.text(
-                human.size(status.speed) + '/c' + ' (' + human.size(status.chunk) + ')'
-            );
-            nodes.indicators.time.text(
-                human.time(status.time.elapsed) + ' / ' + human.time(status.time.estimate)
-            );
-        };
-        static size() {
-            let size = upload.getSize();
-            let percent = Math.round(size * 100 / file.size);
-            nodes.indicators.progress.css('width',  percent + '%')
-                .text(human.size(size) + ' (' + percent + '%)');
-        };
-    }
+    // Дії при призупиненні/продовжені/відміні процесу завантаження файлу
+    nodes.buttons.pause.click(() => {upload.pause()});
+    nodes.buttons.resume.click(() => {upload.resume()});
+    nodes.buttons.cancel.click(() => {upload.stop()});
 });
 
 
@@ -136,8 +148,7 @@ $.fn.disable = function() {return this.prop('disabled', true)};
 
 
 // Клас для виводу розміру файлу та інтервалу часу в зручному для людині вигляді
-let human = new function() {
-
+const human = new function() {
     this.size = function(bytes) {
         const thousand = 1000;
         if(Math.abs(bytes) < thousand) return bytes + ' B';
