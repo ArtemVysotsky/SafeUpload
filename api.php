7613943c47b7111c55dfe716d345cbe5b678b4df
Artem Visotsky <a.vysotsky@gmail.com>
2020-02-26 01:18:20 +0200

    rem empty lines from api.php

--- api.php
+++ api.php
@@ -10,55 +10,35 @@
 /** ToDo: Перевірити видалення файла при помилці */
 
 set_error_handler('exceptionErrorHandler');
-
 $output = [];
 
 try {
-
     require_once('File.php');
-
     if (!isset($_GET['action'])) throw new Exception('Відсутня дія');
-
     if (!isset($_GET['name'])) throw new Exception('Відсутня назва файлу');
-
     $file = new File($_GET['name']);
 
     try {
-
         if (isset($_POST['hash'])) $file->setHash($_POST['hash']);
-
         switch($_GET['action']) {
-
             case 'open': $output['hash'] = $file->open(); break;
-
             case 'append': {
-
                 if ((count($_FILES) == 0) || (!isset($_FILES['chunk'])))
-
                     throw new Exception('Відсутній фрагмент файла');
-
                 $output['size'] = $file->append($_FILES['chunk'], $_POST['offset']);
-
             } break;
-
             case 'close': $output['size'] = $file->close($_POST['time'] ?? null); break;
-
             case 'remove': $file->remove(); break;
-
             default: throw new Exception('Невідома дія');
         }
 
     } catch (Exception $exception) {
-
         $file->remove();
-
         throw $exception;
     }
 
 } catch (Exception $exception) {
-
     header('HTTP/1.x 500 Internal Server Error');
-
     $output['exception'] = $exception->getMessage();
 }
 
@@ -75,6 +55,5 @@ echo json_encode($output, JSON_UNESCAPED_UNICODE);
  * @throws ErrorException Error to Exception
  */
 function exceptionErrorHandler(int $number, string $string, string $file, int $line) {
-
     throw new ErrorException($string, 0, $number, $file, $line);
 }
